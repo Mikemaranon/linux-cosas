5e110934f18abf98518bb98b87503ca8c3856b0e
Mike <mikemaranonx@gmail.com>
2024-11-14 13:12:08 +0100

    Update jupyter.sh

--- scripts/spark-jupyter/jupyter.sh
+++ scripts/spark-jupyter/jupyter.sh
@@ -8,3 +8,17 @@ pip3 install jupyter-core
 sudo apt install openssh-server -y
 pip3 install jupyterlab
 pip3 install notebook
+
+pip install findspark
+cd ~
+echo "export SPARK_HOME=/opt/spark" >> pyspark.txt
+echo "export PATH=\$SPARK_HOME/bin:\$PATH" >> pyspark.txt
+echo "export PYTHONPATH=\$SPARK_HOME/python:\$PYTHONPATH" >> pyspark.txt
+echo "export PYSPARK_PYTHON=python3" >> pyspark.txt
+
+sudo cat ~/bashPrueba.txt >> ~/.bashrc
+rm ~/bashPrueba.txt
+
+cd ~/linux-cosas/scripts/spark-jupyter
+chmod +x sour.sh
+./sour.sh
