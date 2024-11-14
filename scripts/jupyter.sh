60fbe94ee185f313d82da9f89c7941455816c20c
Mike <mikemaranonx@gmail.com>
2024-11-14 12:47:59 +0100

    Update jupyter.sh

--- scripts/jupyter.sh
+++ scripts/jupyter.sh
@@ -4,5 +4,7 @@ sudo systemctl enable ssh
 sudo systemctl start ssh
 
 sudo apt install python3-pip -y
-pip3 install jupyter-core -y
+pip3 install jupyter-core 
 sudo apt install openssh-server -y
+pip3 install jupyterlab
+pip3 install notebook
