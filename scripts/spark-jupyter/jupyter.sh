bf74c9fafd3a2167bb27b70bd1ae074b824faaee
Mike <mikemaranonx@gmail.com>
2024-11-14 13:43:06 +0100

    Update jupyter.sh

--- scripts/spark-jupyter/jupyter.sh
+++ scripts/spark-jupyter/jupyter.sh
@@ -4,7 +4,7 @@ sudo systemctl enable ssh
 sudo systemctl start ssh
 
 sudo apt install python3-pip -y
-pip3 install jupyter-core 
+sudo apt install jupyter-core -y
 sudo apt install openssh-server -y
 pip3 install jupyterlab
 pip3 install notebook
