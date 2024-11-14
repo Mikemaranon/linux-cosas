8e5be5fa1dd380992fcbacea95ea91015567ae5c
Mike <mikemaranonx@gmail.com>
2024-11-14 09:34:09 +0100

    Update installSpark.sh

--- scripts/installSpark.sh
+++ scripts/installSpark.sh
@@ -9,6 +9,7 @@ sudo apt install python3 -y
 sudo wget -N https://downloads.apache.org/spark/spark-3.5.3/spark-3.5.3-bin-hadoop3.tgz
 sudo tar -xvf spark-3.5.3-bin-hadoop3.tgz
 sudo mv spark-3.5.3-bin-hadoop3 /opt/spark
+sudo mv /opt/spark/spark-3.5.3-bin-hadoop3/* /opt/spark
 
 cd ~
 sudo echo "export SPARK_HOME=/opt/spark" >> ~/bashPrueba.txt
